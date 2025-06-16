6f7dc0adf7dc22d5db6873d5f4bca5464b264558
Ashanth <ashanthganesh@outlook.com>
2025-06-15 21:40:35 -0400

    Completed full stack setup.

---
+++ server/app/app.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+import os
+
+app = FastAPI()
+
+# app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+async def read_root():
+    index_path = os.path.join(os.path.dirname(__file__), '..', '..', 'client', 'views', 'index.html')
+    index_path = os.path.abspath(index_path)
+    return FileResponse(index_path)
